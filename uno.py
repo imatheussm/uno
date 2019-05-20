69fd8a4afa86af15064a70b447eaca5939965893
Igor Matheus Souza Moreira <imatheus.sm@gmail.com>
2019-05-20 14:41:58 -0300

    Verification for pyzero added
    Now, before executing, the program will ask if the user wants to run the graphical version or just the text one. I'll attempt to unify both things under a single file to make maintenance easy.

--- uno.py
+++ uno.py
@@ -1,6 +1,21 @@
 import data_structures as ds
 
-# colors (with an additional set omitting 'black', for its cards are all special only
+use_pgzero=None
+while use_pgzero!="y" and use_pgzero!="n":
+    use_pgzero=input("Use pygamezero? [Y]/n: ").lower()
+    if use_pgzero=="": use_pgzero="y"
+    if use_pgzero!="y" and use_pgzero!="n": print("Unknown option. Try again.")
+
+if use_pgzero=="" or use_pgzero=="y": use_pgzero=True
+elif use_pgzero=="n": use_pgzero=False
+
+if use_pgzero==True:
+    try: import pgzero
+    except:
+        use_pgzero=False
+        raise ImportWarning("pgzero not found. Generating a random game in text mode...")
+
+# colors (with an additional set omitting "black", for its cards are all special only
 ALL_COLORS          = ["black","blue","green","red","yellow"] 
 NORMAL_COLORS       = ALL_COLORS[1:]
 # 1 zero + 2*(1, ..., 9)
@@ -20,41 +35,42 @@ class UnoCard:
     color: string
     card_type: string/int
 
-    >>> card = UnoCard('red', 5)
+    >>> card = UnoCard("red", 5)
     """
     def __init__(self, color, card_type):
         self._validate(color, card_type)
         self.color = color
         self.card_type = card_type
         self.temp_color = None
-        #self.sprite = Actor('{}_{}'.format(color, card_type))
+        try: self.sprite = Actor("{}_{}".format(color, card_type))
+        except: pass
 
     def __repr__(self):
-        return '<UnoCard object>\n{:>7}{}\n{:>7}{}'.format("Color: ",self.color,
+        return "<UnoCard object>\n{:>7}{}\n{:>7}{}".format("Color: ",self.color,
                                                            "Type: ",self.card_type)
 
     def __str__(self):
-        return '{}{}'.format(self.color_short, self.card_type_short)
+        return "{}{}".format(self.color_short, self.card_type_short)
 
     def __format__(self, f):
-        if f == 'full':
-            return '{} {}'.format(self.color, self.card_type)
+        if f == "full":
+            return "{} {}".format(self.color, self.card_type)
         else:
             return str(self)
 
     def __eq__(self, other):
         return self.color == other.color and self.card_type == other.card_type
 
-    def _validate(self, color, card_type):
+    def validate(self, color, card_type):
         """
         Check the card is valid, raise exception if not.
         """
         if color not in ALL_COLORS:
-            raise ValueError('Invalid color')
-        if color == 'black' and card_type not in BLACK_CARD_TYPES:
-            raise ValueError('Invalid card type')
-        if color != 'black' and card_type not in COLOR_CARD_TYPES:
-            raise ValueError('Invalid card type')
+            raise ValueError("Invalid color")
+        if color == "black" and card_type not in BLACK_CARD_TYPES:
+            raise ValueError("Invalid card type")
+        if color != "black" and card_type not in COLOR_CARD_TYPES:
+            raise ValueError("Invalid card type")
 
     @property
     def color_short(self):
@@ -62,13 +78,13 @@ class UnoCard:
 
     @property
     def card_type_short(self):
-        if self.card_type in ('skip', 'reverse', 'wildcard'):
+        if self.card_type in ("skip", "reverse", "wildcard"):
             return self.card_type[0].upper()
         else:
             return self.card_type
 
     @property
-    def _color(self):
+    def color(self):
         return self.temp_color if self.temp_color else self.color
 
     @property
@@ -77,9 +93,7 @@ class UnoCard:
 
     @temp_color.setter
     def temp_color(self, color):
-        if color is not None:
-            if color not in COLORS:
-                raise ValueError('Invalid color')
+        if color is not None and color not in COLORS: raise ValueError("Invalid color")
         self._temp_color = color
 
     def playable(self, other):
@@ -87,8 +101,4 @@ class UnoCard:
         Return True if the other card is playable on top of this card,
         otherwise return False
         """
-        return (
-            self._color == other.color or
-            self.card_type == other.card_type or
-            other.color == 'black'
-        )
+        return self._color==other.color or self.card_type==other.card_type or other.color=="black"
